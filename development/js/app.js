afdaa8ff6a41721016b53e9c3b6fa801caae5c18
janfro17 <jan.kacper.froehlich@gmail.com>
2022-06-09 10:02:28 +0200

    7.1_styled

--- development/js/app.js
+++ development/js/app.js
@@ -6,14 +6,25 @@ const deleteButton = document.querySelectorAll('.action-icon-trash');
 const schedulesSection = document.querySelector('.schedules_box');
 const newPlanSection = document.querySelector('.new_plan');
 const newPlanBtn = document.querySelector('.new_plan_btn');
+const previousButton = document.querySelector('#previous');
+const nextButton = document.querySelector('#next');
+
+const recipesSection = document.querySelector('.recipes_box');
+const newRecipeSection = document.querySelector('.new_recipes');
 
 addButton.addEventListener('click', () => {
-schedulesSection.classList.add('hide');
-newPlanSection.classList.remove('hide');
+// schedulesSection.classList.add('hide');
+// newPlanSection.classList.remove('hide');
+recipesSection.classList.add('hide');
+newRecipeSection.classList.remove('hide');
 });
 
 newPlanBtn.addEventListener('click', () => {
     newPlanSection.classList.add('hide');
     schedulesSection.classList.remove('hide');
 });
+
+// previousButton.addEventListener('click', () => {
+// //
+// })
 
